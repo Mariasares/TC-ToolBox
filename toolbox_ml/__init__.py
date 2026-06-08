88516947892f7827691c7f161c9aaf5ef771b029
Mariasares <mariaesteras@gmail.com>
2026-06-08 09:58:16 +0200

    Agregado el notebook demostración

--- toolbox_ml/__init__.py
+++ toolbox_ml/__init__.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+# Para que el programa ubique la carpeta raíz del proyecto
+# para que entienda las rutas absolutas de 'toolbox_ml'
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 """
 Toolbox ML consiste en paquete de herramientas automatizadas para Análisis Exploratorio de Datos (EDA)
 y selección de características orientadas a modelos de regresión.
@@ -7,14 +15,18 @@ Archivo destinado a definir la versión de la librería y para importar las func
 # Definimos la versión del paquete
 __version__ = "0.1.0"
 
-# Importamos las funciones
-from .eda.core import (
+# Importamos las funciones del Dev. 1
+from toolbox_ml.eda.core import (
     describe_df,
-    tipifica_variables,
-    # get_features_num_regression,
-    # plot_features_num_regression,
-    # get_features_cat_regression,
-    # plot_features_cat_regression
+    tipifica_variables
+)
+
+# 2. Importamos las funciones de Claudia (desde la carpeta feature_selection)
+from toolbox_ml.eda.core_desarrollador_2 import (
+    get_features_num_regression,
+    plot_features_num_regression,
+    get_features_cat_regression,
+    plot_features_cat_regression
 )
 
 # Definimoas qué funciones se exportan públicamente
